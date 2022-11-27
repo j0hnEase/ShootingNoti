70364be2e90e682ce97e2b39004356cd524842cf
j0hnEase <118790889+j0hnEase@users.noreply.github.com>
2022-11-28 01:59:13 +0800

    Update STNWindow.h

--- ShootingNoti/src/STNWindow.h
+++ ShootingNoti/src/STNWindow.h
@@ -3,8 +3,18 @@
 
 @interface STNWindow : UIWindow
 
+@property (nonatomic, copy) void (^tapAction) (void);
+@property (nonatomic, copy) void (^leftBtnAction) (void);
+@property (nonatomic, copy) void (^rightBtnAction) (void);
+
 /// To show text
 - (void)showText:(NSString *)text;
+
+- (void)showImage:(UIImage *)image artId:(NSString*)artId;
+- (void)hideImage:(BOOL)hide;
+
+- (void)showPermanentText:(NSString *)text;
+- (void)hidePermanentText:(BOOL)hide;
 
 
 @end
